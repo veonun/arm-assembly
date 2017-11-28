ba8b92e19c64354dd0d8ed35418a384d355e789e
veonun <veonun@ttu.ee>
2017-11-28 09:51:15 +0200

    Add max.c neon instrustion enabled file neonmax.s

---
+++ neonmax.s
@@ -0,0 +1,69 @@
+	.arch armv5t
+	.fpu neon
+	.eabi_attribute 20, 1
+	.eabi_attribute 21, 1
+	.eabi_attribute 23, 3
+	.eabi_attribute 24, 1
+	.eabi_attribute 25, 1
+	.eabi_attribute 26, 2
+	.eabi_attribute 30, 6
+	.eabi_attribute 34, 0
+	.eabi_attribute 18, 4
+	.file	"max.c"
+	.text
+	.align	2
+	.global	max
+	.syntax unified
+	.arm
+	.type	max, %function
+max:
+	@ args = 0, pretend = 0, frame = 16
+	@ frame_needed = 1, uses_anonymous_args = 0
+	@ link register save eliminated.
+	str	fp, [sp, #-4]!
+	add	fp, sp, #0
+	sub	sp, sp, #20
+	str	r0, [fp, #-16]
+	str	r1, [fp, #-20]
+	ldr	r3, .L6
+	str	r3, [fp, #-12]
+	mov	r3, #0
+	str	r3, [fp, #-8]
+	b	.L2
+.L4:
+	ldr	r3, [fp, #-8]
+	lsl	r3, r3, #2
+	ldr	r2, [fp, #-16]
+	add	r3, r2, r3
+	ldr	r2, [r3]
+	ldr	r3, [fp, #-12]
+	cmp	r2, r3
+	ble	.L3
+	ldr	r3, [fp, #-8]
+	lsl	r3, r3, #2
+	ldr	r2, [fp, #-16]
+	add	r3, r2, r3
+	ldr	r3, [r3]
+	str	r3, [fp, #-12]
+.L3:
+	ldr	r3, [fp, #-8]
+	add	r3, r3, #1
+	str	r3, [fp, #-8]
+.L2:
+	ldr	r2, [fp, #-8]
+	ldr	r3, [fp, #-20]
+	cmp	r2, r3
+	blt	.L4
+	ldr	r3, [fp, #-12]
+	mov	r0, r3
+	sub	sp, fp, #0
+	@ sp needed
+	ldr	fp, [sp], #4
+	bx	lr
+.L7:
+	.align	2
+.L6:
+	.word	-99999
+	.size	max, .-max
+	.ident	"GCC: (Ubuntu/Linaro 5.4.0-6ubuntu1~16.04.4) 5.4.0 20160609"
+	.section	.note.GNU-stack,"",%progbits
